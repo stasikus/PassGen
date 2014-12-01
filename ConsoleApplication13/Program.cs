77c5772f05ed6b80a3ee61304865900bd178de26
Stanislav Kravchenko <stasikusk@gmail.com>
2014-12-01 12:34:47 +0200

    PassGen
    PassGen

---
+++ ConsoleApplication13/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                string myPassword = GUIDGen.GetGUID();
+                Console.WriteLine(myPassword);
+            }
+            Console.ReadLine();
+        }
+
+        public static class GUIDGen
+        {
+            public static string GetGUID()
+            {
+                byte[] bytes = new Byte[16];
+                RNGCryptoServiceProvider rand = new RNGCryptoServiceProvider();
+                rand.GetBytes(bytes);
+                Guid myGuid = new Guid(bytes);
+                return myGuid.ToString().Replace("-", "").Trim();
+            }
+        }
+    }
+}
